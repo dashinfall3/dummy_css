f807329900020122b50289a18406c6b4b36ddabc
Apprentice <apprentice@dbc23.local>
2013-02-21 11:08:11 -0800

    css

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,20 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+get 'page1' do
+  erb :page1
+end
+
+get 'page2' do
+  erb :page2
+end
+
+get 'page3' do
+  erb :page3
+end
+
+get 'page4' do
+  erb :page4
+end
